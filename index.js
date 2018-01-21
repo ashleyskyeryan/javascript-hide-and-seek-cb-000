53883b3ea44d54681f5f58fa8a7ffbc5c893ec0e
Ashley Ryan <ashleyskyeryan@gmail.com>
2018-01-21 18:17:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function deepestChild() {
   }
 }
 
-function increaseRankBy() {
+function increaseRankBy(n) {
   const ranks = document.querySelectorAll(ul.ranked-list 'li');
   for (let i = 0; i < ranks.length; i++) {
   ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n;
