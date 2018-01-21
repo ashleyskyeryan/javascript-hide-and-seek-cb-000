5f49bfe571ba7365bbbf468e9d04dd83f998e699
Ashley Ryan <ashleyskyeryan@gmail.com>
2018-01-21 18:14:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,14 +9,14 @@ function nestedTarget() {
 function deepestChild() {
   const lis = document.getElementById('grand-node').querySelectorAll('div')
   for (let i = 0; i < lis.length; i++) {
-    lis[i].innerHTML = 
+    lis[i].innerHTML =
   }
 }
 
 function increaseRankBy() {
   const ranks = document.getElementByClassName('ranked-list').querySelectorAll('li')
   for (let i = 0; i < ranks.length; i++) {
-  new rank = ranks.parseInt() 
+  new rank = ranks.parseInt()
   rank++
 }
 }
