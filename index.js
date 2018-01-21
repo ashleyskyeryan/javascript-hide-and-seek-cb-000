fc0a81787b4558d8e39ca12872b9a20e728105f7
Ashley Ryan <ashleyskyeryan@gmail.com>
2018-01-21 18:14:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,18 @@ function getFirstSelector(selector) {
 function nestedTarget() {
   return document.querySelector('#nested .target');
 }
+
+function deepestChild() {
+  const lis = document.getElementById('grand-node').querySelectorAll('div')
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = 
+  }
+}
+
+function increaseRankBy() {
+  const ranks = document.getElementByClassName('ranked-list').querySelectorAll('li')
+  for (let i = 0; i < ranks.length; i++) {
+  new rank = ranks.parseInt() 
+  rank++
+}
+}
